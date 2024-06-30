2d19cd0f455d687dc1b19ca9160db2119544e098
Christophe <cguegan@gmail.com>
2024-06-30 15:55:52 +0200

    Initial Commit

---
+++ ChecklistsObservation/ChecklistsObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistsObservationApp.swift
+//  ChecklistsObservation
+//
+//  Created by Christophe Guégan on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistsObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
